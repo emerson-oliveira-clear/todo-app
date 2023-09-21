788f84bd3e97b5a30cd0ce4fbb60135e9ae71219
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-21 10:51:28 -0300

    UPDATE: backend completed

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -1,11 +1,15 @@
-const restful = require('node-restful')
-const mongoose = restful.mongoose
 
-const todoSchema = new mongoose.Schema({
-    description:{type: String,require: true},
-    done:{ type: Boolean ,require: true ,default: flase},
-    createdAt:{type: Date, default:Date.now}
+const mongoose = require('mongoose');
+const {Schema} = mongoose
 
-})
+const todoSchema = new Schema({ 
+    description: { type: String, required: true },
+    done: { type: Boolean, required: true, default: false },
+    createdAt:{ type: Date, default: Date.now }
+ });
 
-module.exports = restful.module('Todo', todoSchema)
+ const Task = mongoose.model('Task', todoSchema)
+
+
+
+module.exports = Task
